d386b0c522e1f2ae72097a8ce5435d036c79053d
Valery Vargin <vdvugad@gmail.com>
2020-09-20 17:11:11 +0300

    Remove unused components in pages

--- src/Page/AnalyticsPage.tsx
+++ src/Page/AnalyticsPage.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => ({}));
 
 export default function AnalyticsPage() {
-  const classes = useStyles();
-
   return (
     <Container>
       <Typography variant="h1" align="center">
