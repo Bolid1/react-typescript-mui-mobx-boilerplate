d6c230f43d1978c3adbc9f2e5286bcb187d1045c
Valery Vargin <vdvugad@gmail.com>
2020-10-02 00:19:06 +0300

    Sticky app bar

--- src/Component/TopMenu.tsx
+++ src/Component/TopMenu.tsx
@@ -1,29 +1,47 @@
-import { Hidden } from "@material-ui/core";
 import React from "react";
+import { Hidden } from "@material-ui/core";
+import createStyles from "@material-ui/core/styles/createStyles";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import { useUIStore } from "Store";
 
-export default function TopMenu() {
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(4),
+    },
+  })
+);
+
+export default function TopMenu({ title }: { title: string }) {
+  const classes = useStyles();
   const { leftMenu } = useUIStore();
 
   return (
-    <Hidden mdUp={true}>
-      <AppBar position="fixed">
-        <Toolbar>
+    <AppBar position="sticky" className={classes.root}>
+      <Toolbar>
+        <Hidden mdUp>
           <IconButton
             edge="start"
             color="inherit"
             aria-label="menu"
+            className={classes.menuButton}
             onClick={leftMenu.toggle}
           >
             <MenuIcon />
           </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </Hidden>
+        </Hidden>
+        <Typography variant="h6" noWrap>
+          {title}
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
