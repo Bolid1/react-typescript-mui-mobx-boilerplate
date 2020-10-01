58db732123b8a1b4363f1b86df3d7429400ddcdc
Valery Vargin <vdvugad@gmail.com>
2020-10-02 00:13:42 +0300

    hotFixes

--- src/Component/__tests__/Main.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Main from "../Main";
-
-test("Main", () => {
-  const { baseElement } = render(<Main />);
-  expect(baseElement).toBeInTheDocument();
-});
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import App from "App";
 
 test("App", () => {
-  const { baseElement } = render(<App />);
+  const { baseElement } = render(
+    <Router>
+      <App />
+    </Router>
+  );
   expect(baseElement).toBeInTheDocument();
 });
